b55b60bce662a355c413bbb0b98556dee6e34191
raejun92 <cvcv1324@naver.com>
2021-08-19 10:09:26 +0900

    맵 처음 또는 사이 개행 예외처리

--- ft_parse.c
+++ ft_parse.c
@@ -17,6 +17,26 @@ int	check_map_name(char *argv)
 	return (1);
 }
 
+// 기능: 맵을 파싱하기전에 맵시작 또는 맵사이에 개행이 있는지 확인, 리턴: void
+static void	check_newline(char *map)
+{
+	int	i;
+	int	len;
+	int	j;
+
+	i = 1;
+	len = ft_strlen(map);
+	j = -1;
+	if (map[0] == '\n')
+		error_msg();
+	while (++j < len)
+	{
+		if (map[i - 1] == '\n' && map[i] == '\n')
+			error_msg();
+		i++;
+	}
+}
+
 // 기능: 인수로 넣은 맵내용을 game구조체에 저장, 리턴: void
 void	map_parse(t_game *game, char *argv)
 {
@@ -36,6 +56,7 @@ void	map_parse(t_game *game, char *argv)
 		free(tmp);
 		tmp = chunk;
 	}
+	check_newline(tmp);
 	game->map = ft_split(tmp, '\n');
 	free(tmp);
 }
